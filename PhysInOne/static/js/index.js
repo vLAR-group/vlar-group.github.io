1c134f9d6f5882f1f5f32ab13f9396dd64d99611
JOE <23123812r@gmail.com>
2026-04-13 22:25:51 +0800

    PhysInOne Web Update

--- PhysInOne/static/js/index.js
+++ PhysInOne/static/js/index.js
@@ -16,7 +16,8 @@ window.PERSONAL_WEBSITES = {
   "Peng Yun": "https://scholar.google.com.hk/citations?user=alRGtgwAAAAJ",
   "Zihui Zhang": "https://scholar.google.com/citations?user=jiwazT8AAAAJ",
   "Shenxing Wei": "https://scholar.google.com.hk/citations?user=5RH0zBMAAAAJ",
-  "Bowen Cheng": "https://scholar.google.com/citations?user=mmFhKOAAAAAJ&hl=en"
+    "Bowen Cheng": "https://scholar.google.com/citations?user=mmFhKOAAAAAJ&hl=en",
+    "Jiahao Chen": "https://github.com/D2Simon"
 };
 
 // Contributors functions
